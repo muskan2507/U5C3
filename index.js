963deaaeaf10fe628f133fb35e1e3a953a13888b
Muskan Shaw <princesahil0065@gmail.com>
2022-06-06 16:06:52 +0530

    sone

--- index.js
+++ index.js
@@ -3,25 +3,42 @@ const fs=require("fs")
 const app=express();
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-// app.get("/",(req,res)=>{
-//     res.send("hello")
-// })
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
 app.post("/user/create",(req,res)=>{
     fs.readFile("./db.json",{encoding:"utf-8"},(err,data)=>{
         const parsed=JSON.parse(data);
-        parsed.users=[...parsed.users,req.body]
-        fs.writeFile("./db.json",JSON.stringify(parsed),"utf-8",()=>{
-            res.status(201).send("user created")
+        console.log(req.body)
+        let d=  JSON.parse(data).users
+        d.map((e)=>{
+            if(e.name !== req.body.name){
+                parsed.users=[...parsed.users,req.body]
+                fs.writeFile("./db.json",JSON.stringify(parsed),"utf-8",()=>{
+                    res.status(201).send("user created")
+                })
+            }
         })
+       
     })
 })
-app.get("/",(req,res)=>{
+app.get("/user/:id",(req,res)=>{
     const {id} = req.params;
     fs.readFile("./db.json",{encoding:"utf-8"},(err,data)=>{
-        
-       res.send(JSON.parse(data))
+            
+
+     let d=  JSON.parse(data).users
+    // console.log(d)
+    d.map((e)=>{
+        console.log(e)
+        if(e.id==id){
+            res.send(e)
+        }
+    
+    })
     })
 })
+app.post()
 app.listen(8080,()=>{
     console.log("server running at port http://localhost:8080/*")
 })

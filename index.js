765c8364c581f5265d28d0a6b8bdbf1c7942ed87
Muskan Shaw <princesahil0065@gmail.com>
2022-06-06 16:52:20 +0530

    done

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const { v4: uuidv4 } = require("uuid");
 const express=require("express")
 const fs=require("fs")
 const app=express();
+const { validate, ValidationError, Joi } = require('express-validation');
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+const loggerValidation = {
+    body: Joi.object({
+        username: Joi.string().required(),
+        password: Joi.string().required()
+    })
+  }
+
 app.get("/",(req,res)=>{
     res.send("hello")
 })
 app.post("/user/create",(req,res)=>{
+    let c=0;
+    const id=c++
+    req.body = {...req.body,id}
+
     fs.readFile("./db.json",{encoding:"utf-8"},(err,data)=>{
         const parsed=JSON.parse(data);
         console.log(req.body)
@@ -33,12 +47,44 @@ app.get("/user/:id",(req,res)=>{
         console.log(e)
         if(e.id==id){
             res.send(e)
-        }
+        }   
     
     })
     })
 })
-app.post()
+app.post("/user/login",validate(loggerValidation, {}, {}),(req,res)=>{
+    
+    const token = uuidv4();
+    fs.readFile("./db.json","utf-8",(err,data)=>{
+        const parsed = JSON.parse(data);
+        parsed.users.map((el)=>{
+            if(el.username==req.body.username && el.password==req.body.password){
+                el.token=token;
+            }
+        })
+        parsed.users = [...parsed.users];
+
+        fs.writeFile("./db.json",JSON.stringify(parsed),{encoding:"utf-8"},()=>{
+            res.status(201).send("Login successfull");
+        })
+    })
+})
+
+app.post("/user/logout",(req,res)=>{
+    fs.readFile("./db.json","utf-8",(err,data)=>{
+        const parsed = JSON.parse(data);
+        parsed.users.map((el)=>{
+            if(el.username==req.body.username && el.password==req.body.password){
+                el.token="";
+            }
+        })
+        parsed.users = [...parsed.users];
+
+        fs.writeFile("./db.json",JSON.stringify(parsed),{encoding:"utf-8"},()=>{
+            res.status(200).send("user logged out successfully");
+        })
+    })
+}) 
 app.listen(8080,()=>{
     console.log("server running at port http://localhost:8080/*")
 })
